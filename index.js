2ade8a182dbc8d23e25429630eb58cfe9852f97a
sriram022005 <151645746+sriram022005@users.noreply.github.com>
2023-11-21 20:03:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,49 +1,86 @@
-window.onload = function () {
-    // Load previous data from localStorage
-    loadPreviousData();
-  };
+// Event listener for DOMContentLoaded event
+document.addEventListener('DOMContentLoaded', () => {
+  // Get the reference to the user table body
+  const userTableBody = document.getElementById('userTableBody');
 
-  function loadPreviousData() {
-    var savedData = JSON.parse(localStorage.getItem('registrationData'));
-    if (savedData) {
-      // Add previous data to the table
-      addRowToTable(savedData);
+  // Loop through all localStorage items
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+
+    // Check if the key starts with 'user_'
+    if (key.startsWith('user_')) {
+      // Parse the JSON-stored user data from localStorage
+      const userData = JSON.parse(localStorage.getItem(key));
+
+      // Add the user data to the table
+      addRowToTable(userTableBody, userData);
     }
   }
+});
 
-  function submitForm() {
-    // (Your existing submitForm function remains unchanged)
-    // ...
+// Event listener for form submission
+const form = document.getElementById('registrationForm');
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent default form submission
 
-    // Clear form fields
-    document.getElementById("registrationForm").reset();
+  // Get the user's date of birth from the form
+  const dob = new Date(form.dob.value);
 
-    // Load previous data from localStorage and add to the table
-    loadPreviousData();
+  // Calculate the user's age based on the current year
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - dob.getFullYear();
+
+  // Validate the user's age to ensure it's between 18 and 55
+  if (!(age >= 18 && age <= 55)) {
+    alert('Age should be between 18 and 55.');
+    return;
   }
 
-  function addRowToTable(data) {
-    var table = document.getElementById("dataTable");
-    var row = table.insertRow(1);
+  // Generate a unique user key using the current timestamp
+  const userKey = 'user_' + Date.now();
 
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
+  // Create a user object with the form data
+  const userData = {
+    name: form.name.value,
+    email: form.email.value,
+    password: form.password.value,
+    dob: form.dob.value,
+    acceptedTerms: form.acceptedTerms.checked,
+  };
 
-    cell1.innerHTML = data.username;
-    cell2.innerHTML = data.email;
-    cell3.innerHTML = data.password;
-    cell4.innerHTML = data.dob;
+  // Store the user data in localStorage using the generated key
+  localStorage.setItem(userKey, JSON.stringify(userData));
 
-    // Display "Yes" if terms are accepted, "No" otherwise
-    cell5.innerHTML = data.acceptedTerms ? "Yes" : "No";
-  }
+  // Add the user data to the table
+  addRowToTable(userTableBody, userData);
+});
 
-  function isValidAge(dob) {
-    var dobDate = new Date(dob);
-    var currentDate = new Date();
-    var age = currentDate.getFullYear() - dobDate.getFullYear();
-    return age >= 18 && age <= 55;
-  }
+// Function to add a new user row to the table
+function addRowToTable(tableBody, userData) {
+  // Create a new table row
+  const newRow = tableBody.insertRow();
+
+  // Set a consistent style for all cells
+  const cellStyle = 'border border-gray-300 p-2';
+
+  // Add cells for each user data property
+  const nameCell = newRow.insertCell();
+  nameCell.textContent = userData.name;
+  nameCell.className = cellStyle;
+
+  const emailCell = newRow.insertCell();
+  emailCell.textContent = userData.email;
+  emailCell.className = cellStyle;
+
+  const passwordCell = newRow.insertCell();
+  passwordCell.textContent = userData.password;
+  passwordCell.className = cellStyle;
+
+  const dobCell = newRow.insertCell();
+  dobCell.textContent = userData.dob;
+  dobCell.className = cellStyle;
+
+  const acceptedTermsCell = newRow.insertCell();
+  acceptedTermsCell.textContent = userData.acceptedTerms;
+  acceptedTermsCell.className = cellStyle;
+}
